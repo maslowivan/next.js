maslowivan/next.js

commit 22d0b54d4cc6b7ec0fd5e9f785049d5c32431d09

Turbopack: Update notify-rs crate, remove workaround for fixed bug (#81909)
8.0.0 had a bug that was fixed in 8.1.0 that required a workaround for error codes: https://github.com/notify-rs/notify/issues/685

Tested on Linux with

```
rm -rf /tmp/fuzz && RUST_BACKTRACE=1 cargo run --release -p turbo-tasks-fuzz -- fs-watcher --fs-root /tmp/fuzz --directory-modifications 0
```

turbopack/crates/turbo-tasks-fs/src/watcher.rs
@@ -10,7 +10,7 @@ use std::{
     time::Duration,
 };
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 use notify::{
     Config, EventKind, PollWatcher, RecommendedWatcher, RecursiveMode, Watcher,
     event::{MetadataKind, ModifyKind, RenameMode},
@@ -113,20 +113,6 @@ impl DiskWatcher {
         dir_path: &Path,
         root_path: &Path,
     ) -> Result<()> {
-        use anyhow::Context;
-        // HACK: Rewrite NotFound io errors to PathNotFound
-        // This shouldn't ever happen (notify should transform this for us), but even after
-        // https://github.com/notify-rs/notify/pull/611, it seems like there's still some case where
-        // it can occur with inotify on Linux.
-        fn map_notify_err(mut err: notify::Error) -> notify::Error {
-            if let notify::ErrorKind::Io(io_err) = &err.kind
-                && io_err.kind() == std::io::ErrorKind::NotFound
-            {
-                err.kind = notify::ErrorKind::PathNotFound;
-            }
-            err
-        }
-
         if let Some(watcher) = watcher.as_mut() {
             let mut path = dir_path;
             let err_with_context = |err| {
@@ -136,10 +122,7 @@ impl DiskWatcher {
                     path.display()
                 ));
             };
-            while let Err(err) = watcher
-                .watch(path, RecursiveMode::NonRecursive)
-                .map_err(map_notify_err)
-            {
+            while let Err(err) = watcher.watch(path, RecursiveMode::NonRecursive) {
                 match err {
                     notify::Error {
                         kind: notify::ErrorKind::PathNotFound,

Cargo.toml
@@ -379,7 +379,7 @@ napi = { version = "2", default-features = false, features = [
     "napi5",
     "compat-mode"
 ] }
-notify = "8.0.0"
+notify = "8.1.0"
 once_cell = "1.17.1"
 owo-colors = "3.5.0"
 parcel_selectors = "0.28.2"

commit b05d6d764216ffe22b0c802384fdda26f8841d6e

Turbopack: Move fs watcher anyhow::Context import inline to fix compilation warnings (#81997)
I broke this in my previous PR

https://github.com/vercel/next.js/pull/81909/files#r2225998847

turbopack/crates/turbo-tasks-fs/src/watcher.rs
@@ -10,7 +10,7 @@ use std::{
     time::Duration,
 };
 
-use anyhow::{Context, Result};
+use anyhow::Result;
 use notify::{
     Config, EventKind, PollWatcher, RecommendedWatcher, RecursiveMode, Watcher,
     event::{MetadataKind, ModifyKind, RenameMode},
@@ -113,6 +113,8 @@ impl DiskWatcher {
         dir_path: &Path,
         root_path: &Path,
     ) -> Result<()> {
+        use anyhow::Context; // inner import due to conditional compilation
+
         if let Some(watcher) = watcher.as_mut() {
             let mut path = dir_path;
             let err_with_context = |err| {